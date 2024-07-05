3849d3fe02a4afc37de346016f5e6f920c95bd3e
Alan Ancona Ortiz <167805532+aanconao@users.noreply.github.com>
2024-07-05 18:58:47 +0200

    character.ts

--- ExamenExtraordinarioDefinitivo/resolvers/Query/character.ts
+++ ExamenExtraordinarioDefinitivo/resolvers/Query/character.ts
@@ -2,7 +2,7 @@ import { GraphQLError } from "graphql";
 import { CharactersFromAPI } from "../../types.ts";
 
 export const character = async (id: string): Promise<CharactersFromAPI> => {
-  const url = "https://rickandmortyapi.com/api/character/" + id;
+  const url = ("https://rickandmortyapi.com/api/character/" + id);
   const response = await fetch(url);
   if (response.status != 200)throw new GraphQLError("Error para adquirir la informacion del personaje por ese id");
   return await response.json() as CharactersFromAPI;
